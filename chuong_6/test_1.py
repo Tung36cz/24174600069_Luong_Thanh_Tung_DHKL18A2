943b3a67a44bf471b90f979ef09890250d3e4bd6
Tung36cz <lttung.24174600069@sv.uneti.edu.vn>
2024-10-15 20:48:47 +0700

    bai 1

---
+++ chuong_6/test_1.py
@@ -0,0 +1,25 @@
+a = 1
+b = 3
+
+di_hoc=1
+
+DI_HOC =2
+abc1 =2
+
+# giai pt bac nhat ax+b=c
+he_so_a =1
+he_so_a =3
+he_so_c =2
+
+bien_int = 3 #dinh dang int(interger) - kieu so nguyen: ...-2, -1, 0, 1,2,...
+bien_float = 4.678 #ding dang float - kieu so thuc: ..-2.3,-2.324,...,0,1.0,1.1
+bien_string = "Day la kieu ky tu" #dinh dang str (strign) - kieu ky tu:abcdef
+kieu_boolean_1  =True #ding dang boot (boolean) - kieu lua chon True, False
+kiu_booleam_2 = False
+kieu_none = None #dinh dang khai bao khong dinh dang
+kieu_list = [] #dinh dang list (danh sach) - kieu du lieu tuan tu dang danh sach: {1,"abc",2}
+kieu_dictinary ={"khoa":"gia tri","a":1} #dinh dang kieu dict ( tu dien)
+kieu_set= {1,2,"abc"} #dinh dang set (tap hop)
+
+
+print(type(bien_float))
